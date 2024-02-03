9855e5c6660c6f01f57864cccbf46a63a4303165
Güven Fazlı <guven.fazli@gmail.com>
2024-02-03 14:58:20 +0300

    Cart / modal opening fclosing and rendering in cart is done

--- 12-starting-project/src/Components/Header.jsx
+++ 12-starting-project/src/Components/Header.jsx
@@ -1,8 +1,20 @@
 import logo from '../assets/logo.jpg'
 import Button from './UI/Button'
+import { useContext } from "react"
+import CartContext from "../store/CartContext"
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header(){
 
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity
+  }, 0);
+  const userProgressCtx = useContext(UserProgressContext)
+
+  function handleShowCart(){
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -12,7 +24,7 @@ export default function Header(){
       </div>
 
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
       </nav>
     </header>
 
