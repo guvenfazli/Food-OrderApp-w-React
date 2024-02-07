682044548ca45c0179960bbd1da8d1444f3620cd
Güven Fazlı <guven.fazli@gmail.com>
2024-02-07 22:17:54 +0300

    We got back to useState

--- 12-starting-project/src/Components/HeaderMenu.jsx
+++ 12-starting-project/src/Components/HeaderMenu.jsx
@@ -1,5 +1,4 @@
 import { useState, useContext, useReducer } from "react"
-import CartContext from "../store/CartContext"
 
 
 const dropList = {
@@ -14,25 +13,21 @@ export default function HeaderMenu({ children }) {
 
   const [drop, setDrop] = useState(false)
 
-  const cartCtx = useContext(CartContext)
 
-  const styleList = cartCtx.testRed ? 'active-drop' : 'inactive-drop'
+  const styleList = drop ? 'active-drop' : 'inactive-drop'
 
-  function testFunctionNow(){
-    cartCtx.testReducer();
-  }
+ 
 
-  console.log(cartCtx.testRed)
  
   
 
   return (
-    <div onMouseOver={testFunctionNow} onMouseLeave={() => setDrop(false)} style={{display: 'flex', flexDirection: 'column', position: 'relative', border: '1px solid red', width: '180px'}}>
+    <div onMouseOver={() => setDrop(true)} onMouseLeave={() => setDrop(false)} style={{display: 'flex', flexDirection: 'column', position: 'relative', border: '1px solid red', width: '180px'}}>
       <button>{children}</button>
 
       <div onMouseOver={() => setDrop(true)} onMouseLeave={() => setDrop(false)}  className={styleList}>
         {dropList[children].map((button) => <button key={button}>{button}</button>)}
-        <button onClick={testFunctionNow}>Test</button>
+       
 
       </div>
 
